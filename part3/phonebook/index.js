a501f6365e6a816f89285c19c0f9539c82d246ca
SpyFox <wasyldykun@gmail.com>
2022-08-13 14:53:44 +0100

    3.2 finished

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,3 +1,4 @@
+const { request, response } = require("express")
 const express = require("express")
 
 const app = express()
@@ -27,6 +28,11 @@ const persons = [
 
 app.get('/api/persons', (request, response) => {
     response.send(persons)
+})
+
+app.get('/info', (request, response) => {
+    response.send(`<div>Phonebook has info for ${persons.length} people</div>` 
+    + `<div>${new Date()}</div>`)
 })
 
 const PORT = 3001
