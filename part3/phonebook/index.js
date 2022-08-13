a0abd0eb59d9bb6746a295249cd934ce3f5f0f7d
SpyFox <wasyldykun@gmail.com>
2022-08-13 15:58:42 +0100

    3.8 fixed

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -3,7 +3,14 @@ const express = require("express")
 const morgan = require("morgan")
 
 const app = express()
-morgan.token('json', (req, res) => JSON.stringify(req.body))
+morgan.token('json', (req, res) => {
+    if (req.method === 'POST') {
+        return JSON.stringify(req.body)
+    }
+    else {
+        return null
+    }
+})
 
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :json'))
