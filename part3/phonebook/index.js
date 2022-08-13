0d50bf8c404359e6b0461f05db8415142a9b2c25
SpyFox <wasyldykun@gmail.com>
2022-08-13 15:55:20 +0100

    3.8 finished

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -3,8 +3,11 @@ const express = require("express")
 const morgan = require("morgan")
 
 const app = express()
+morgan.token('json', (req, res) => JSON.stringify(req.body))
+
 app.use(express.json())
-app.use(morgan('tiny'))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :json'))
+
 let persons = [
     {
         "id": 1,
